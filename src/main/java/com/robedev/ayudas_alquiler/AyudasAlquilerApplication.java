0f30fed42f7bbbcaa6be887d61bc0e1331410e8d
Roberto Vetere <Roberto.vetere@gmail.com>
2024-06-06 16:28:21 +0200

    first commit

---
+++ src/main/java/com/robedev/ayudas_alquiler/AyudasAlquilerApplication.java
@@ -0,0 +1,18 @@
+package com.robedev.ayudas_alquiler;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyudasAlquilerApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyudasAlquilerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
